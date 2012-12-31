436c6e6e8eb3e2ada644a456e74da0d03b440e91
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-12-31 03:48:05 +0300

    ffmpeg_simple stub

---
+++ ffmpeg_simple.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "ffmpeg_simple.h"
+
+unsigned long* ffmpegsimple_readfirstframe(const char* filename, int *width, int *height) {
+    return NULL;
+}
---
+++ ffmpeg_simple.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* 
+ * Open the the file with FFmpeg, read the first video frame, convert it to RGBA image.
+ * 
+ * returns malloc'ed buffer with image on success and NULL on failure
+ */
+unsigned long* ffmpegsimple_readfirstframe(const char* filename, int *width, int *height);
