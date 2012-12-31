f1ecb1206adf452dd9559fcd8df707c932742fc2
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-12-31 04:53:03 +0300

    Use ffmpeg_simple in the plugin.

--- loader_ffmpeg.c
+++ loader_ffmpeg.c
@@ -39,9 +39,7 @@
 #include <string.h>
 #include <fcntl.h>
 
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libswscale/swscale.h>
+#include "ffmpeg_simple.h"
 
 #include <Imlib2.h>
 
@@ -49,74 +47,20 @@
 #include "loader.h"
 
 
-static int ffmpeg_already_initialized = 0;
-
 char load(ImlibImage * im, ImlibProgressFunction progress,
           char progress_granularity, char immediate_load)
 {
   
   int w,h;
-  int has_alpha = 1;
-  char retret = 0;
-
-  //fprintf(stderr, "Qqq %s\n", im->real_file);
-  
   
   if(im->data)
     return 0;
 
-  if (!ffmpeg_already_initialized) {
-    avcodec_register_all();
-    av_register_all();
-    ffmpeg_already_initialized = 1;
-  }
+  unsigned long* decoded_image = ffmpegsimple_readfirstframe(
+      im->real_file, &w, &h);
+  int decoded_image_used = 0;
   
-  AVFormatContext *pFormatCtx = NULL;
-  if (avformat_open_input(&pFormatCtx, im->real_file, NULL, NULL)!=0) {
-    perror("av_open_input_file");
-    return 0;
-  }
-
-  if(avformat_find_stream_info(pFormatCtx, NULL)<0) {
-    perror("avformat_find_stream_info");
-    return 0;
-  }
-  
-  int i;
-  AVStream* videoStream = NULL;
-  int videoStreamIndex = 0;
-  for(i=0; i<pFormatCtx->nb_streams; i++) {
-    AVStream *r = pFormatCtx->streams[i];
-    if(r->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-      if (!videoStream) {
-        videoStream = r;
-        videoStreamIndex = i;
-      } else {
-        fprintf(stderr, "Warning: ignoring other video streams\n");
-      }
-    }
-  }
-  if (!videoStream) {
-    avformat_close_input(&pFormatCtx);
-    return 0;
-  }
-  
-  // Find the decoder for the video stream
-  AVCodec* pCodec = avcodec_find_decoder(videoStream->codec->codec_id);
-  if(pCodec==NULL) {
-    goto EXIT;
-  }
-
-   // Open codec
-  if(avcodec_open2(videoStream->codec, pCodec, NULL)<0){
-    goto EXIT;
-  }
-    
-  
-  w = videoStream->codec->width;
-  h = videoStream->codec->height;
-
-  fprintf(stderr, "w=%d h=%d\n", w, h);
+  if (!decoded_image) return 0;
   
   if(!im->loader && !im->data) {
     im->w = w;
@@ -125,119 +69,26 @@ char load(ImlibImage * im, ImlibProgressFunction progress,
     if(!IMAGE_DIMENSIONS_OK(w, h))
       goto EXIT;
 
-    if(!has_alpha)
-      UNSET_FLAGS(im->flags, F_HAS_ALPHA);
-    else
-      SET_FLAGS(im->flags, F_HAS_ALPHA);
+    SET_FLAGS(im->flags, F_HAS_ALPHA);
     
     im->format = strdup("ffmpeg");
   }
 
   if((!im->data && im->loader) || immediate_load || progress) {
-    AVPacket packet;
-    AVPacket packet_to_send;
-    av_init_packet(&packet);
-    packet.size=0;
-    packet_to_send.size=0;
-    AVFrame* frame = avcodec_alloc_frame();
-    AVFrame* frameRGB = avcodec_alloc_frame();
-    
-    
-    int limit=256;
-    int got_frame = 0;
-    int no_more_frames=0;
-    for(;;) {
-      int ret;
-        
-      if (!no_more_frames) {
-        read_next_packet:
-        ret = av_read_frame(pFormatCtx, &packet);
-        if(ret<0) {
-          fprintf(stderr, "No more frames\n");
-          no_more_frames=1;
-          //break;
-        }
-      }
-      fprintf(stderr, "ret %p %p %d\n", videoStream->codec, packet.data, packet.size);
-      if (packet.stream_index != videoStream->index) {
-        goto read_next_packet;
-      }
-        
-      
-      int isFrameAvailable;
-      ret = avcodec_decode_video2(videoStream->codec, frame, &isFrameAvailable, &packet);
-      if (ret < 0) {
-        fprintf(stderr, "avcodec_decode_video2 failed\n");
-        break; // fail
-      }
-      
-      if (isFrameAvailable) {
-        got_frame = 1;
-        break;
-      }
-      
-      --limit;
-      if(!limit) {
-        fprintf(stderr, "Limit reached\n");
-        break;
-      }
-    }
-    av_free_packet(&packet);
-    
-    if (got_frame) {
-    
-      fprintf(stderr, "Got frame %dx%d\n", frame->width, frame->height);
-    
-      int numBytes=avpicture_get_size(PIX_FMT_BGRA, w, h);
-      if (numBytes< w*h*4) {
-        fprintf(stderr, "Warning: requested buffer size appears to be too small\n");
-        numBytes = w*h*4;
-      }
-      unsigned char* buffer = malloc(numBytes);
-
-      // Assign appropriate parts of buffer to image planes in pFrameRGB
-      avpicture_fill((AVPicture *)frameRGB, buffer, PIX_FMT_BGRA, w, h);
-    
-      struct SwsContext *img_convert_ctx = sws_alloc_context();
-      img_convert_ctx = sws_getCachedContext(
-        img_convert_ctx, 
-        w, h, videoStream->codec->pix_fmt, 
-        w, h, PIX_FMT_BGRA, 
-        SWS_BICUBIC, NULL, NULL, NULL);
-
-       if (img_convert_ctx == NULL) {
-        fprintf(stderr, "Cannot initialize the conversion context\n");  
-        avcodec_free_frame(&frame);  
-        avcodec_free_frame(&frameRGB);
-        goto EXIT;
-       }
-       
-      sws_scale(img_convert_ctx, 
-       (const unsigned char*const*)frame->data, frame->linesize, 0, videoStream->codec->height,
-       frameRGB->data, frameRGB->linesize);
-    
-      im->data = buffer;//malloc(4*w*h);
-      
-      retret = 1;
-      if(progress)
-        progress(im, 100, 0, 0, w, h);
-    }
-    
-    avcodec_free_frame(&frame); 
-    avcodec_free_frame(&frameRGB);
-    
+    im->data = (DATA32*)decoded_image;
+    decoded_image_used = 1;
+    if(progress)
+      progress(im, 100, 0, 0, w, h);
+    return 1;
   }
 
   
 
 EXIT:
-  avformat_close_input(&pFormatCtx);
-  if(!retret) {
-    fprintf(stderr, "R\n");
-    im->w = 0;
-    im->h = 0;
+  if (!decoded_image_used) {
+    free(decoded_image);
   }
-  return retret;
+  return 0;
 }
 
 char save(ImlibImage *im, ImlibProgressFunction progress,
